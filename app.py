d8467d7496ca4c9b047f4c04c92f1771c925f211
jaguarhari-07 <jaguar.xyz999@gmail.com>
2026-08-25 20:59:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def scan_commodity():
 
         # Updated to gemini-2.5-flash
         response = ai_client.models.generate_content(
-            model='gemini-3.6-flash',
+            model='gemini-2.5-flash',
             contents=[img_part, prompt],
             config=types.GenerateContentConfig(
                 response_mime_type="application/json"
